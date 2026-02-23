85a8b97051bed864de7996d35e62d0150873913e
Namhoang211205 <namhoang211205@gmail.com>
2026-02-23 14:34:39 -0500

    feat: Adding Contact Info

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,30 @@
 import React from 'react'
+const contactInfo = [
+  {
+    icon: Phone,
+    label: 'Email',
+    value: 'namhoang211205@gmail.com',
+    href: 'mailto:namhoang211205@gmail.com'
+  },
+  {
+    icon: Mail,
+    label: 'Email',
+    value: 'nhoang@oberlin.edu',
+    href: 'mailto:nhoang@oberlin.edu'
+  },
+  {
+    icon: Phone,
+    label: 'Phone',
+    value: '+1 (440) 581-4056',
+    href: 'tel:+14405814056'
+  },
+  {
+    icon: MapPin,
+    label: 'Location',
+    value: 'Oberlin, OH',
+    href: '#'
+  }
+];
 
 export const Contact = () => {
   return (
